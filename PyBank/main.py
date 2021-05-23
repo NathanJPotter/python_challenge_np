d0dd07983e11918b94cb68103eac97c78e109f7f
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-05-23 11:54:42 +1000

    Update main.py
    Defined os paths to read csv file and write text file.
    
    Started to plan out steps for writing code.

---
+++ PyBank/main.py
@@ -0,0 +1,54 @@
+# This is a script to run a financial analysis of budget_data for the PyBank exercise
+
+# Import the os module for creating file paths across operating systems. And import the module for reading CSV files
+
+import os
+import csv
+
+# Specify os path for reading csv file
+
+csv_path = os.path.join('..', 'Resources', 'budget_data.csv')
+print(csv_path)
+
+# Specify os path for writing text file
+
+text_path = os.path.join('..', 'Analysis', 'financial_analysis.txt')
+print(text_path)
+
+# Give text file the title 'Financial Analysis'
+
+
+# Calculate total number of months
+
+
+    # Print and write to text file the total number of months
+
+
+# Calculate net total profit/losses over period
+
+
+    # Print and write to text file the total profit/losses
+
+
+# Calculate the changes in profit/losses over the period
+
+
+
+# Calculate the average of changes in profits/losses
+
+
+    # Print and write to text file the average of changes in profits/losses
+
+
+# Calculate the greatest increase in profits
+
+
+    # Print and write to text file the greatest increase in profits
+
+
+# Calculate the greatest decrease in losses
+
+
+    # Print and write to text file the greatest decrease in losses
+
+    

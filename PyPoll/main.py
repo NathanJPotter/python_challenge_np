78c792811660f5422c6dd57a5445fbf45d5e5d1c
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-05-27 12:25:45 +1000

    Total Votes is done and prints

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,22 +8,59 @@ import csv
 # Specify os path for reading csv file
 
 csv_path = os.path.join('..', 'PyPoll/Resources', 'election_data.csv')
-print(csv_path)
 
 # Specify os path for writing text file
 
 text_path = os.path.join('..', 'PyPoll/Analysis', 'election_results.txt')
-print(text_path)
+
+# Create lists to hold data
+
+votes = []
+
+
+
+# Open and read CSV file
+
+with open(csv_path, newline="") as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=",")
+
+    # Skip the header row
+    next(csvreader)
+
+    # Loop through csv file
+
+    for row in csvreader:
+
+        # Add voting choices to votes list
+        votes.append(row[2])
+
+
+# Get the total number of votes cast
+
+total_votes = (len(votes))
+
+# Retrieve the full list of candidates who received votes
+
+
+
+# Calculate the percentage of votes each candidate won
+
+
+
+# Calculate the number of votes each candidate won
+
+
+
+# Retrieve the winer of the election based on number of votes
+
+
 
 # Give text file the title 'Election Results'. Print.
 
-title = "Election Results"
-
-print(title)
-
-title_line = "------------------------------""------------------------------"
-
-print(title_line)
+print("Election Results")
+print("------------------------------")
+print("Total Votes: " + str(total_votes))
+print("------------------------------")
 
 # Write title to text file
 
@@ -31,3 +68,4 @@ text_file_title = ["Election Results \n", "------------------------------ \n"]
 
 with open(text_path, 'w') as f:
     f.writelines(text_file_title)
+
